a1513b529fefd6d7df64d3396f9b1c9b95c4024d
rylemacaraig <rylemacaraig@gmail.com>
2019-11-23 18:02:02 -0500

    Initial Commit

---
+++ MyWeatherTvApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeatherTvApp
+//
+//  Created by Ryle  Macaraig on 2019-11-23.
+//  Copyright © 2019 Ryle  Macaraig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
